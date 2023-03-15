58090e999bbf9e5b898fe2a88c7a5fff5d27ab61
Saltssaumure <saltssaumure@gmail.com>
2023-03-15 18:59:49 +0000

    Add scss compiler

---
+++ scss-compile.config.js
@@ -0,0 +1,13 @@
+/** @type {import('bd-scss/lib/config').Config} */
+export default {
+    meta: {
+        /** The official name of your theme. */
+        name: "Synthesis",
+        /** The name of your base scss file. */
+        scss: "synth",
+        /** The name of your theme repository. */
+        repo: "synthesis-discord-theme",
+        /** The version of your theme. */
+        version: "1.2.1"
+    }
+};
